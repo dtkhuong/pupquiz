72135fd5ddef8102af9acd6e7c14cafeaa2cbc3a
Diana <diana@Dianas-MacBook-Pro.local>
2019-07-23 15:12:40 -0700

    Bring to current buggy version

--- memory1.js
+++ memory1.js
@@ -1,9 +1,75 @@
-var cards = document.querySelectorAll(".memory-card");
+var cards = document.querySelectorAll('.memory-card');
+var hasFlippedCard = false;
+var lockBoard = false;
+var firstCard, secondCard;
+var resetButton = document.querySelector('.reset');
+var currentScore = 0;
 
-function flipCard(){
-    console.log("i was clicked");
-    console.log(this + "is memory-card");
-    this.classList.toggle('flip');
+
+function shuffle(){
+    // console.log('shuffled');
+    for (var i = 0 ; i < cards.length; i++){
+        var randomPos = Math.floor(Math.random()* 20);
+        cards[i].style.order = randomPos;
+        console.log(randomPos);
+    }
+};
+
+resetButton.addEventListener('click', shuffle);
+ 
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+    if (!hasFlippedCard){
+        //first click
+            hasFlippedCard = true;
+            firstCard = this;
+        //second click
+    } else {
+            hasFlippedCard = false;
+            secondCard = this;
+            console.log(this);
+        //do cards match?
+            if (firstCard.dataset.name === secondCard.dataset.name){
+                //it's a match --> disable the card by removing event listener
+                    firstCard.removeEventListener('click', flipCard);
+                    secondCard.removeEventListener('click', flipCard);
+                    resetBoard();
+                    currentScore += 1;
+                    console.log(currentScore);
+            } else {
+                //not a match --> lock the board, unflip the card by removing the flip class, then unlock the board
+                    lockBoard = true;
+                    setTimeout(() => {
+                        firstCard.classList.remove('flip');
+                        secondCard.classList.remove('flip');
+                        resetBoard();
+                    }, 1500);
+                    currentScore += 1;
+                    console.log(currentScore);
+                    
+            }fm
+    }
+    document.querySelector('#currentScore').textContent =  currentScore;
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+
+
+for (var i = 0; i < cards.length; i++){
+    cards[i].addEventListener('click', flipCard)
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    for (var i = 0 ; i < cards.length; i++){
+        var randomPos = Math.floor(Math.random()* 20);
+        cards[i].style.order = randomPos;
+        console.log(randomPos);
+    }
+})();
